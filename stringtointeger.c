014ddb02d979f133e8ee884b72c86f2f96640ecc
keerthivivekanandhan <30337466+keerthivivekanandhan@users.noreply.github.com>
2017-08-11 16:45:35 +0530

    Create stringtointeger.c

---
+++ stringtointeger.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int c,n,i;
+    char a[10];
+    scanf("%d",&n);
+    scanf("%s",&a[i]);
+    for(i=0;i<=n;i++)
+    {
+    if(a[i]>='a'&&a[i]<='z'||a[i]>='A'&&a[i]<='Z')
+    {
+        printf("%d",a[i]);
+    }
+    }
+
+    return 0;
+}
